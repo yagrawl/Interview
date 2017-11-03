d61621cce8ab85874deaadcbd01ed656467b6a56
Yash Agrawal <yagrawl2@gmail.com>
2017-11-02 21:11:15 -0500

    changed to leetcode vars, sol1 not working

--- Leetcode JS/q001.js
+++ Leetcode JS/q001.js
@@ -1,35 +1,40 @@
 
 // O(n^2) - My solution
-function find_pair(num_array, target) {
-	for(let i = 0; i < num_array.length; i++) {
+function find_pair(nums, target) {
+	for(let i = 0; i < nums.length; i++) {
 		// Creating an array from i to end with complements
-		let curr_num = num_array.slice(i).map(function(value) {
+		let curr_num = nums.slice(i).map(function(value) {
 			return target - value;
 		});
 
 		// If complement is in remaining array, return
-		if(curr_num.indexOf(num_array[i]) >= 0) {
-			console.log([i, i + curr_num.indexOf(num_array[i])]);
+		if(curr_num.indexOf(nums[i], i) >= 0) {
+			console.log([i, i + curr_num.indexOf(nums[i])]);
 		}
 	}
 }
+// There's a bunch of stuff wrong with my solution
+// Leetcode suggests to not make functions within a loop
+// Also, I'm only passing this one test case that I've written below
+// There's some problem with this
 
 // O(n) solution - Hash table single pass
 // Space O(n)
-function find_pair_efficient(num_array, target) {
+function find_pair_efficient(nums, target) {
 	// Creating a dictionary 
 	let complement_mapper = {};
-	for(let i = 0; i < num_array.length; i++) {
+	for(let i = 0; i < nums.length; i++) {
 		// If complement already exists, return
-		if(num_array[i] in complement_mapper) {
-			console.log([complement_mapper[num_array[i]], i]);
+		if(nums[i] in complement_mapper) {
+			console.log([complement_mapper[nums[i]], i]);
 		}
 		else {
 			// If not then add
-			complement_mapper[target - num_array[i]] = i;
+			complement_mapper[target - nums[i]] = i;
 		}
 	}
 }
+// This solution works and is better than 99.18% JS submissions
 
 find_pair([2, 7, 11, 15], 9);
 find_pair_efficient([7, 8, 11, 2], 9);
