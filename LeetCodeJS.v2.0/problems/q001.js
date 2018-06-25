cc37706b56b45607fc01855f98abc8454647d96e
Yash Agrawal <yagrawl2@gmail.com>
2018-06-24 22:26:53 -0700

    q001

--- LeetCodeJS.v2.0/problems/q001.js
+++ LeetCodeJS.v2.0/problems/q001.js
@@ -1,5 +1,10 @@
-function twoSum(nums) {
-    console.log(1);
+var twoSum = function(num, target) {
+    index_dir = {};
+    num.forEach((elt, i) => {index_dir[elt] = i});
+    for(let i = 0; i < num.length; i++) {
+        let val = target - num[i];
+        if(typeof index_dir[val] !== 'undefined' && index_dir[val] !== i) {return [i, index_dir[val]]};
+    }
 }
 
-twoSum(1);
+console.log(twoSum([2, 7, 10, 12], 9));
